7f9f77056d493db288533fcb76d6c65e23dd62fb
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-01-26 15:13:29 +0000

    Initial Commit

---
+++ KoriApi/KoriApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoriApiApp.swift
+//  KoriApi
+//
+//  Created by Abdoulaye Aliou SALL on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct KoriApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
